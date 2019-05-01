f454de1665a827d5c40d5a6ad30255a41acf7b61
Paras Prajapti <paras.praj@icloud.com>
2019-05-01 18:45:24 -0400

    Added content to pages

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,14 +1,13 @@
 $(document).ready(() => {
     let contactForm = $('#contactForm');
-    let formSuccessAlert = $('#formSuccessAlert');
     let formSubmitButton = $('#formSubmitButton');
     contactForm.submit((event) => {
         event.preventDefault(); 
         console.log(contactForm.serialize());
+        formSubmitButton.prop('disabled', true);
         formSubmitButton.html('Please Wait...');
         setTimeout(() => {
-            formSubmitButton.html('Send');
-            formSuccessAlert.show();
+            formSubmitButton.html('Message Sent!');
         }, 2000);
     });
 });
