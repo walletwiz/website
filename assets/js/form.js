0f114e17c42e461db4cdd31742bd406b791b472e
Paras Prajapti <paras.praj@icloud.com>
2019-05-01 20:37:15 -0400

    added success and danger alerts for form submit

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,13 +1,18 @@
 $(document).ready(() => {
     let contactForm = $('#contactForm');
     let formSubmitButton = $('#formSubmitButton');
+    let formSubmitSuccess = $('#formSubmitSuccess');
+    let formSubmitDanger = $('#formSubmitDanger');
     contactForm.submit((event) => {
         event.preventDefault(); 
         console.log(contactForm.serialize());
         formSubmitButton.prop('disabled', true);
         formSubmitButton.html('Please Wait...');
         setTimeout(() => {
-            formSubmitButton.html('Message Sent!');
+            formSubmitButton.html('Send');
+            formSubmitButton.prop('disabled', false);
+            formSubmitSuccess.show();
+            formSubmitDanger.hide();
         }, 2000);
     });
 });
