8de842e8803c0a4943c2ea518c3b0a71a9e38fb3
Paras Prajapti <paras.praj@icloud.com>
2019-05-02 20:11:31 -0400

    set up contact form handler

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,18 +1,35 @@
 $(document).ready(() => {
+    
     let contactForm = $('#contactForm');
     let formSubmitButton = $('#formSubmitButton');
     let formSubmitSuccess = $('#formSubmitSuccess');
     let formSubmitDanger = $('#formSubmitDanger');
+
     contactForm.submit((event) => {
-        event.preventDefault(); 
-        console.log(contactForm.serialize());
+        event.preventDefault();
+
         formSubmitButton.prop('disabled', true);
         formSubmitButton.html('Please Wait...');
-        setTimeout(() => {
-            formSubmitButton.html('Send');
-            formSubmitButton.prop('disabled', false);
-            formSubmitSuccess.show();
-            formSubmitDanger.hide();
-        }, 2000);
+        formSubmitSuccess.hide();
+        formSubmitDanger.hide();
+
+        $.ajax({
+            type: "POST",
+            url: 'https://walletwiz-website-back-end.herokuapp.com/api/handle-contact-form',
+            data: contactForm.serialize(),
+            success: function(data) {
+                formSubmitButton.html('Send');
+                formSubmitButton.prop('disabled', false);
+                formSubmitSuccess.show();
+                formSubmitDanger.hide();
+            },
+            error: function(xhr) {
+                formSubmitButton.html('Send');
+                formSubmitButton.prop('disabled', false);
+                formSubmitDanger.html(xhr.responseText);
+                formSubmitSuccess.hide();
+                formSubmitDanger.show();
+            }
+        });
     });
 });
